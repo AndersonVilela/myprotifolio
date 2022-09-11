7f6d3bafb07449952599855fe34d1c8e72d61704
Anderson Vilela <andersonvilela.al@gmail.com>
2022-09-11 00:12:07 -0300

    commmit

--- src/pages/Start/index.jsx
+++ src/pages/Start/index.jsx
@@ -6,12 +6,12 @@ export const Start = () => {
 
   return (
     <div className={Styles.Container} id="Start">
-      <div>
+      <div className={Styles.esquerda}>
         <h2 className={Styles.Second}>Olá, eu sou</h2>
         <h1 className={`${Styles.typing} ${Styles.animation} `}>Anderson Vilela :)</h1>
         <h2 className={Styles.Second}>Desenvolvedor Front-end</h2>
       </div>
-      <div>
+      <div className={Styles.direita}>
         <img src={Image} />
 
       </div>
